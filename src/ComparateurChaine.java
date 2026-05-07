3e923d8b0329ecaf25db42873ce784d778a47cfa
dhiyaeddineelgabsi <dhiyaguebsi@gmail.com>
2026-05-07 11:35:10 +0100

    rakaht les document

--- src/ComparateurChaine.java
+++
@@ -1,9 +0,0 @@
-public class ComparateurChaine {
-
-    public ComparateurChaine{
-
-    }
-    public double comparerChaine(String s1, String s2) {
-
-    }
-}
--- src/src/Nom.java
+++ src/src/Nom.java
@@ -2,11 +2,14 @@ public class Nom {
     private int id;
     private String nom;
     private String nomComplet;
+    private java.util.List<String> nomPretraite = new java.util.ArrayList<>();
+    private String sourceList;
 
     public Nom(int id, String nom, String nomComplet) {
         this.id = id;
         this.nom = nom;
         this.nomComplet = nomComplet;
+        this.nomPretraite.add(nom);
     }
 
     public int getId() {
@@ -19,6 +22,17 @@ public class Nom {
 
     public String getNomComplet() {
         return nomComplet;
+    }
+
+    public String getDernierNomPretraite() {
+        if (nomPretraite.isEmpty()) {
+            return null;
+        }
+        return nomPretraite.get(nomPretraite.size() - 1);
+    }
+
+    public String getSourceList() {
+        return sourceList;
     }
 
 
